2ec92ddad1ec259b928b1bbc2cf913a861617ba9
Faxriddin <faxa@mail.ru>
2022-10-03 12:55:58 +0500

    boss

---
+++ main.js
@@ -0,0 +1,8 @@
+var lider = prompt('Ismingizni kiriting')
+if(lider == 'Faxriddin' || lider == 'faxriddin' || lider == 'FAXRIDDIN' || lider == 'Sanjar'){
+    alert('Hello BOSS')
+}else if( lider == 'Kamol' || lider == 'kamol' || lider == 'KAMOL'){
+    alert('Hello guest')
+}else{
+    alert('do not enter');
+}
